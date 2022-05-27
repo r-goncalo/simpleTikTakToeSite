4d42c6b0b8e77457f978fdb2c75a62c98984413f
r-goncalo <r.goncalo@campus.fct.unl.pt>
2022-05-27 09:50:56 +0100

    the setup is complete i think

--- make.sh
+++ make.sh
@@ -1,8 +1,8 @@
 sudo cp index.html /var/www/html/index.html;
-sudo cp -r code /var/www/html/code;
+sudo cp -r code /var/www/html/;
 cd sub;
 for f in *
 do
 echo $f;
-sudo cp -r $f /var/www/html/$f
+sudo cp -r $f /var/www/html/
 done
